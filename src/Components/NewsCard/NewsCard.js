63e74aa0901ef00439f7dcca5a2d7319d129a682
komal <komal.govinda@neosoftmail.com>
2024-03-16 17:46:34 +0530

    add validation for null error

--- src/Components/NewsCard/NewsCard.js
+++ src/Components/NewsCard/NewsCard.js
@@ -17,11 +17,11 @@ export default class NewsCard extends Component {
   async updateNews() {
     this.props.setProgress(10);
     //  let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}&category=${this.props.category}`;
-    let url = `  https://newsdata.io/api/1/news?apikey=pub_35924ad8448eb14892735a427ed558d180c07&country=${this.props.country}&q=${this.props.category}`;
+    let url = `https://newsdata.io/api/1/news?apikey=pub_35924ad8448eb14892735a427ed558d180c07&country=${this.props.country}&q=${this.props.category}`;
 
     let data = await fetch(url);
     let parseData = await data.json(data);
-    console.error(parseData);
+   
 
     this.props.setProgress(50);
     if (parseData.status === 'success') {
@@ -35,10 +35,9 @@ export default class NewsCard extends Component {
       });
     }
 
-
+    console.log(parseData);
     this.props.setProgress(100);
   }
-
   componentDidMount() {
     this.updateNews();
   }
@@ -50,8 +49,9 @@ export default class NewsCard extends Component {
 
   handleNextClick = async () => {
     await this.setState({ page: this.state.page + 1 });
-    this.updateNews(); console.log('next');
+    this.updateNews(); 
   }
+ 
   render() {
     return (
       <>
@@ -64,7 +64,13 @@ export default class NewsCard extends Component {
               
                 return <div className='col-md-4' key={element.title}>
                   {/* <NewsItems title={element.title} description={element.description} ImgUrl={element.urlToImage} readmore={element.url} author={element.author} publishedAt={element.publishedAt} source={element.source.name} /> */}
-                  <NewsItems title={element.title} description={element.description} ImgUrl={element.image_url} readmore={element.link} author={element.creator} publishedAt={element.pubDate} source={element.source_id} />
+                  <NewsItems title={element.title??''} 
+                  description={element.description??''} 
+                  ImgUrl={element.image_url??''} 
+                  readmore={element.link??''} 
+                  author={element.creator??''} 
+                  publishedAt={element.pubDate??''} 
+                  source={element.source_id??''} />
                 </div>
               })
             }
